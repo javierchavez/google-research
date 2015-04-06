4ab93dc7e912ea1421bfc18afc4adfaedb3af1a9
Javier C <youracow@gmail.com>
2015-04-06 15:48:57 -0600

    modeling

---
+++ utils/fetch_to_db.py
@@ -0,0 +1,21 @@
+
+
+# normalize permissions
+# add permissions to model
+
+
+# require preservation of object
+# add app to model
+
+# add images to app
+# add comments to app
+
+
+# associate permissions to app
+# maybe we can get or create permission instance here
+# instad of getting permissions first
+
+
+
+
+
