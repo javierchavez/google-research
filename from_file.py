398f8ccb31775add2e51e68e3c9088731fd05b70
Javier C <youracow@gmail.com>
2015-04-19 01:26:06 -0600

    iterating through all search terms

--- from_file.py
+++ from_file.py
@@ -8,19 +8,29 @@ except NameError:
 
 import json
 import os
+import operator
 
 fp = open('datasets/2k13_all_by_term.json', 'r')
 android_apps = json.load(fp)
 fp.close()
 
 hamming = compare.Hamming()
- 
-hamming.bin_transform(android_apps['facebook'], 'permissions')
-print(hamming.hamming_dist(10))
+
+# get single search term
+# hamming.bin_transform(android_apps['facebook'], 'permissions')
+# print(hamming.hamming_dist(10))
 
 # mapd_sums = hamming.map_names(hamming.sums())
-
 # print(json.dumps(mapd_sums, indent=4))
 
+stats = {}
+# show all search terms
+for key in android_apps.keys():
+    hamming.bin_transform(android_apps[key], 'permissions')
+    stats[key] = hamming.hamming_dist(10)
 
+sl = sorted(stats.items(), key=operator.itemgetter(1), reverse=True)
 
+# top 10 search that show most sparatic use of permissions
+for stat in sl[:10]:
+    print(stat[0] + ": %.2f" % stat[1])
