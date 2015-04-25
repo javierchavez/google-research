d39e21cf37e4293efe7d73a19d64f6582216347b
Javier C <youracow@gmail.com>
2015-04-24 23:35:43 -0600

    New way of grabbing apps from store. This is closely related to django app's models. Current implementation used to query google play store api and write to file based on a single search term/

---
+++ fetch_to_file.py
@@ -0,0 +1,49 @@
+# If module is laoded then reload it otherwise
+# catch the exception and import the module
+try:
+    googleplaystore
+except NameError:
+    from storeapi import googleplaystore
+else:
+    print("problem with gps module")
+    
+import json
+import os
+
+
+__author__ = 'Javier Chavez'
+__email__ = 'javierc@cs.unm.edu'
+
+fp = open('apps.out.json', 'w')
+
+default_value = ''
+# get the vars defined in bashrc
+cookie = {
+    "PLAY_PREFS": os.getenv('PLAY_PREFS', default_value),
+    "NID": os.getenv('NID', default_value),
+    "_gat": os.getenv('GAT', default_value),
+    "_ga":os.getenv('_GA', default_value)
+}
+
+search_term = 'google'
+
+# init google play store api
+ps = googleplaystore.PlayStore(cookie=cookie)
+
+# holder for app objects
+apps_array = []
+# search and get the results
+apps = ps.search(search_term, 1).get_results()
+# iterate through the apps populate
+# populate apps fields and convert to dict
+
+for app in apps:
+    app.populate_fields()
+    apps_array.append(app.to_dict())
+
+
+search_term_obj = {
+    search_term: apps_array
+}
+
+fp.write(json.dumps(search_term_obj, indent=4))
