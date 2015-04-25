ba67ebb5853ffc952557706988994f6b5bc1e90a
Javier C <youracow@gmail.com>
2015-04-24 23:39:14 -0600

    starting point of going to DB

--- utils/fetch_to_db.py
+++ utils/fetch_to_db.py
@@ -16,6 +16,30 @@
 # instad of getting permissions first
 
 
+try:
+    compare
+    reload(compare)
+except NameError:
+    from utils import compare
+
+import json
+import os
+import operator
+import sqlite3
+
+__author__ = 'Javier Chavez'
+__email__ = 'javierc@cs.unm.edu'
+
+fp = open('datasets/2k13_all_by_term.json', 'r')
+db = 'app/google_play/db.sqlite3'
+android_apps = json.load(fp)
+fp.close()
+
+acc = []
+for search_term in android_apps.keys():
+    for app in android_apps[search_term]:
+        acc.append(app)
 
 
-
+conn = sqlite3.connect(db)
+cursor = conn.cursor()        
